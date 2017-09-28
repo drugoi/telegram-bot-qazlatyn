e742226ceae9374bb99ce1884b7d88390e0552a4
Nikita Bayev <drugoi.nik@gmail.com>
2017-09-28 10:05:35 +0600

    Migrate qqDb to qazlatyn-db package

--- transform-string.js
+++ transform-string.js
@@ -1,11 +1,14 @@
-const qqMap = require('./qq-db.js');
+const qazLatyn = require('qazlatyn-db');
 
 const transformString = text => {
   let result = '';
+  let isUpperCase = false;
   if (text.length) {
     for (const element of text) {
-      if (element && typeof qqMap[element] === 'string') {
-        result += qqMap[element];
+      const qlElement = qazLatyn[element.toLowerCase()];
+      if (element && typeof qlElement === 'string') {
+        isUpperCase = element !== element.toLowerCase();
+        result += isUpperCase ? qlElement.charAt(0).toUpperCase() + qlElement.slice(1) : qlElement;
       } else {
         result += element;
       }
