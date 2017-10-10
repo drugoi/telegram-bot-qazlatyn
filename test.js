4d7b25bb4b2879bd68ff9a748590939807f0dd20
Nikita Bayev <drugoi.nik@gmail.com>
2017-10-10 11:50:52 +0600

    Update qazlatyn-db && fixed tests

--- test.js
+++ test.js
@@ -5,34 +5,34 @@ const transformString = require('./transform-string.js');
 test('string transforming lowercase', t => {
   t.is(
     transformString('қанағаттандырылмағандықтарыңыздан'),
-    'qanaghattandyrylmaghandyqtaryngyzdan'
+    'qanag`attandyrylmag`andyqtaryn`yzdan'
   );
 });
 
 test('string transforming capitalize', t => {
   t.is(
     transformString('Қанағаттандырылмағандықтарыңыздан'),
-    'Qanaghattandyrylmaghandyqtaryngyzdan'
+    'Qanag`attandyrylmag`andyqtaryn`yzdan'
   );
 });
 
 test('string transforming uppercase', t => {
   t.is(
     transformString('ҚАНАҒАТТАНДЫРЫЛМАҒАНДЫҚТАРЫҢЫЗДАН'),
-    'QANAGhATTANDYRYLMAGhANDYQTARYNgYZDAN'
+    'QANAG`ATTANDYRYLMAG`ANDYQTARYN`YZDAN'
   );
 });
 
 test('full alphabet', t => {
   t.is(
     transformString('аәбвгғдеёжзийкқлмнңоөпрстуұүфхһцчшщъыіьэюя'),
-    'aaebvgghdejozhzijkqlmnngooeprstwuuefhhcchshshyiejwja'
+    'aa`bvgg`dejojzi`i`kqlmnn`oo`prsty`uu`fhhcc`s`s`yiieiuia'
   );
 });
 
 test('full alphabet capitalize', t => {
   t.is(
     transformString('АӘБВГҒДЕЁЖЗИЙКҚЛМНҢОӨПРСТУҰҮФХҺЦЧШЩЪЫІЬЭЮЯ'),
-    'AAeBVGGhDEJoZhZIJKQLMNNgOOePRSTWUUeFHHCChShShYIEJwJa'
+    'AA`BVGG`DEJoJZI`I`KQLMNN`OO`PRSTY`UU`FHHCC`S`S`YIIeIuIa'
   );
 });
